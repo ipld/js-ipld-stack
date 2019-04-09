bd064ad0d3ab5fa5cf8a072723d44dcc5a783562
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-04-09 20:33:03 +0000

    fix: speed up pure nodejs interface

--- src/get-codec/index-nodejs.js
+++ src/get-codec/index-nodejs.js
@@ -1,16 +1,13 @@
-const cache = {}
+const implementations = {
+  'dag-cbor': require('../dag-cbor'),
+  'raw': require('../raw')
+}
 
 /* temp getFormat until the real one is implemented */
-const getCodec = async codec => {
-  if (cache[codec]) return cache[codec]
-  if (codec === 'dag-cbor') {
-    cache[codec] = require('../dag-cbor')
-  }
-  if (codec === 'raw') {
-    cache[codec] = require('../raw')
-  }
-  if (cache[codec]) return cache[codec]
+const getCodec = codec => {
+  if (implementations[codec]) return implementations[codec]
   else throw new Error(`Unknown codec ${codec}`)
 }
 
 module.exports = getCodec
+
